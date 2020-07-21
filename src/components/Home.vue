5eed57c594117e12e4a217d0d26391d401e7f2c3
QuanMiny <944307891@qq.com>
2020-07-21 17:04:30 +0800

    实现菜单栏简单功能和欢迎页面

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,142 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/shu.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主题区域 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#6495ED" text-color="#fff" active-text-color="#FFFF00" unique-opened :collapse="isCollapse" :collapse-transition="false" router>
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-menu"></i>
+              <!-- 文本 -->
+              <span>{{ subItem.authName }}</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧主体内容 -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      // 左侧菜单栏数据
+      menulist: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+      },
+      isCollapse: false
+    }
+  },
+  // 页面刚一加载时就应该立即获取
+  created () {
+    this.getMenulist()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有菜单
+    async getMenulist() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
+    // 点击按钮，切换菜单折叠
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
 
-</style>>
+.el-header {
+  background-color: #4169E1;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      width: 16%;
+    }
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #6495ED;
+  // 多了border使得侧边栏不齐
+  .el-menu {
+    border-right: 0;
+  }
+}
+
+.el-main {
+  background-color: #FFFAFA;
+}
+
+/deep/ .el-submenu__title i, .el-menu-item i {
+    color: #fff;
+}
+
+.iconfont {
+  margin-right: 10px;
+  color: #fff;
+}
+
+.toggle-button {
+  background-color: #87CEFA;
+  font-size: 14px;
+  line-height: 30px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2rem;
+  cursor: pointer;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,30 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Header,
+  Aside,
+  Container,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem
+} from 'element-ui'
 
+// 全局挂载
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
-// 全局挂载
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Container)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+
 Vue.prototype.$message = Message
